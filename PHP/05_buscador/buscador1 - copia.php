c317d7c05480c43326931644e408ea68ac000d54
Miguel Esteban <miguelestebn@gmail.com>
2026-02-09 14:01:33 +0100

    a

---
+++ PHP/05_buscador/buscador1 - copia.php
@@ -0,0 +1,9 @@
+<h1>Buscador</h1>
+<form action="https://google.com/search">
+
+    <label>
+        <input type="text" name='q' placeholder="Escribe térmno a buscar">
+    </label>
+    <input type="submit" value="Buscar">
+
+</form>
