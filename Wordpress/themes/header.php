167b7ed461f146bd8a78a4a41dd2f290a8dd011a
Miguel Esteban <miguelestebn@gmail.com>
2026-03-06 12:57:14 +0100

    Add header.php for WordPress theme structure

---
+++ Wordpress/themes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>">
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title(); ?></title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <?php
+    wp_head();
+    // Indicamos WP que terminamo el el Head por si ha de meter algun contenido extra (por ejemplo para plugins o componentes - inspecciona la web para ver como aquí se ha añadido contenido) ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <header>
+        <a href="<?= get_home_url() ?>"><? wp_title() ?></a>
+        <nav>
+            <ul>
+                <li><a href="#">Apartado</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
