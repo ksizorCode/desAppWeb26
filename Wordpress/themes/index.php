6e5b4075cc5bfa0941602034090d829e147ba59b
Miguel Esteban <miguelestebn@gmail.com>
2026-03-06 13:26:44 +0100

    Update index.php

--- Wordpress/themes/index.php
+++ Wordpress/themes/index.php
@@ -1,7 +1,12 @@
 <? get_header()?>
-        
+        <div class="sumario">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
             <article>
+                <?php if (has_post_thumbnail()) : ?>
+    <div class="post-thumbnail">
+        <?php the_post_thumbnail('large'); // Puedes usar 'thumbnail', 'medium', 'large' o 'full' ?>
+    </div>
+<?php endif; ?>
                 <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                 <?php the_excerpt(); ?>
             </article>
@@ -9,5 +14,5 @@
             <p>No hay contenido disponible.</p>
         <?php endif; ?>
 
-
+</div>
 <?php get_footer(); ?>
