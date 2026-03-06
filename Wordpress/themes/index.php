80a6a7f46e63b92b6595a7f55fc25108db39ae2a
Miguel Esteban <miguelestebn@gmail.com>
2026-03-06 13:09:59 +0100

    Update index.php

--- Wordpress/themes/index.php
+++ Wordpress/themes/index.php
@@ -1,4 +1,13 @@
 <? get_header()?>
-        <h1>Aquí va tu contenido</h1>
-        <p>Tu "mierda" ahora estará bien envuelta en HTML.</p>
+        
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <article>
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <?php the_excerpt(); ?>
+            </article>
+        <?php endwhile; else : ?>
+            <p>No hay contenido disponible.</p>
+        <?php endif; ?>
+
+
 <?php get_footer(); ?>
