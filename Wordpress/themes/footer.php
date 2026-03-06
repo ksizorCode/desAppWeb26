a8694edc07e4d3c0130feab5f1b185185505d513
Miguel Esteban <miguelestebn@gmail.com>
2026-03-06 12:57:30 +0100

    Create footer.php with footer content
    Add footer section with copyright and wp_footer() call.

---
+++ Wordpress/themes/footer.php
@@ -0,0 +1,10 @@
+</main>
+    <footer>
+    <p>&copy; <?get_bloginfo( 'name' ) ?></p>
+    <?
+    wp_footer();
+    // Indicamos WP que terminamo el footer por si ha de meter algun contenido extra (por ejemplo para plugins o componentes - inspecciona la web para ver como aquí se ha añadido contenido)
+    ?>
+    </footer>
+</body>
+</html>
